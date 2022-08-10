959281f890252c5140e2bc02484df2b81b742cb7
Sam <snippetsbysam@outlook.com>
2022-08-10 22:15:47 +0100

    add grid initialiser to game logic

--- BattleshipLiteLibrary/GameLogic.cs
+++ BattleshipLiteLibrary/GameLogic.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BattleshipLiteLibrary.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,45 @@ namespace BattleshipLiteLibrary
 {
     public static class GameLogic
     {
+        public static void InitializeGrid(PlayerInfoModel model)
+        {
+            List<string> letters = new List<string>
+            {
+                "A",
+                "B",
+                "C",
+                "D",
+                "E"
+            };
+
+            List<int> numbers = new List<int>
+            {
+                1,
+                2,
+                3,
+                4,
+                5
+            };
+
+            foreach (string letter in letters)
+            {
+                foreach (int number in numbers)
+                {
+                    AddGridSpot(model, letter, number);
+                }
+            }
+        }
+
+        private static void AddGridSpot(PlayerInfoModel model, string letter, int number)
+        {
+            GridSpotModel spot = new GridSpotModel
+            {
+                SpotLetter = letter,
+                SpotNumber = number,
+                Status = GridSpotStatus.Empty
+            };
+
+            model.ShotGrid.Add(spot);
+        }
     }
 }
